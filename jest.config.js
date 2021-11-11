268795f2a9104cb9244fb2be9acb85d33b306e99
Carl Blockås <carlblockas@gmail.com>
2021-11-11 18:12:09 +0100

    Initial commit with base files and configuration

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: '@vue/cli-plugin-unit-jest',
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!lodash-es)',
+  ],
+};
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'PP Shaper',
+    },
+  },
+  devServer: {
+    overlay: {
+      warnings: false,
+      errors: false,
+    },
+  },
+};
